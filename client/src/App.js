b74936fcb6a2943dace118898b284bfafa72febb
Dhairya Singla <97469815+dhairyasingla09@users.noreply.github.com>
2023-06-25 00:14:55 +0530

    Update App.js
    Added Comments

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,13 @@ import {
 import { v4 as uuidV4 } from "uuid"
 
 function App() {
+  //first redirect route is to redirect to a brand new document using unique id 
+  //second redirect route is to match path of documents to the any id
   return (
     <Router>
       <Switch>
-        <Route path="/" exact>
-          <Redirect to={`/documents/${uuidV4()}`} />
+        <Route path="/" exact >
+          <Redirect to={`/documents/${uuidV4()}`} />  
         </Route>
         <Route path="/documents/:id">
           <TextEditor />
